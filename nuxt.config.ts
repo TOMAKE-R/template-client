173a15bf811450daab4b7ae73aa194f3cdf367c4
rastik1337 <rastik12315@seznam.cz>
2023-01-17 00:06:28 +0100

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'TOMAKE Template',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+        },
+      ],
+    },
+  },
+  srcDir: 'src/',
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/apollo'],
+  typescript: {
+    shim: false,
+  },
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'https://graphqlzero.almansi.me/api',
+      },
+    },
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+    configPath: 'tailwind.config.js',
+  },
+});
